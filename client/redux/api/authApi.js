926fb4f36d25abf2553feac77444594d7592cbdd
Lalitmahata77 <162821750+Lalitmahata77@users.noreply.github.com>
2024-07-18 12:28:37 +0545

    Update authApi.js

--- client/redux/api/authApi.js
+++ client/redux/api/authApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
     reducerPath : "authApi",
-    baseQuery : fetchBaseQuery({baseUrl : "/api/v2"}),
+    baseQuery : fetchBaseQuery({baseUrl : "https://facebook-p4gz.onrender.com"}),
     endpoints : (builder)=>({
         register : builder.mutation({
             query(body) {
